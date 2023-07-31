6c147aff8588e748673dcc68fdf58252dfdd081b
Avik Panja <emailforavik@gmail.com>
2023-07-31 17:25:15 +0530

    Initial commit

---
+++ src/main/java/com/cache/lru/Cache.java
@@ -0,0 +1,109 @@
+package com.cache.lru;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class Cache {
+
+	private static final long DEAULT_SIZE_CONSTRAINT = 4;
+	
+	private List<Entry> cache;
+	private long capacity;
+	private long size = 0;
+	
+	class Entry implements Comparable<Entry> {
+		String key;
+		String value;
+		
+		public Entry(String key, String value) {
+			this.key = key; this.value = value;
+		}
+		
+		public int compareTo(Entry another) {
+			System.out.println("compareTo called");
+			return this.key.compareTo(another.key);
+		}
+		
+		public boolean equals(Entry another) {
+			System.out.println("equals called");
+			return this.key == another.key;
+		}
+		@Override
+		public String toString() {
+			return key;
+		}
+	}
+		
+	public Cache() {
+		this.cache = new LinkedList<>();
+		this.capacity = DEAULT_SIZE_CONSTRAINT;
+	}
+	
+	public long size() { return size; }
+	
+	public Optional<String> getData(String key) {
+		final int index[] = new int[] {-1}; 
+		Optional<String> optData = Optional.empty();
+		Optional<Entry> optEntry = this.cache
+				.stream()
+				.filter(entry -> {
+					++index[0];
+					return entry.key.equals(key);
+				})
+				.findAny();
+		if(optEntry.isPresent()) {
+			if(index[0]!=this.size-1) {
+				this.cache.remove(optEntry.get());
+				this.cache.add(optEntry.get());
+			}
+			optData = Optional.of(optEntry.get().value);
+		} 
+		return optData;
+	}
+	
+	public void storeData(String key, String value) {
+		final int index[] = new int[] {-1}; 
+		
+		Optional<Entry> optEntry = this.cache
+				.stream()
+				.filter(entry -> {
+					++index[0];
+					return entry.key.equals(key);
+				})
+				.findAny();
+		if(optEntry.isEmpty()) {
+			if(this.size==this.capacity) {
+				this.cache.remove(0);
+			} else {
+				++size;
+			}
+			this.cache.add(new Entry(key, value));
+		}
+	} 
+	
+	@Override
+	public String toString() {
+		
+		return this.cache.toString();
+	}
+	
+	public static void main(String[] args) {
+		Cache c = new Cache();
+		System.out.println(c.size());
+		
+		c.storeData("Name", "Avik");
+		c.storeData("Roll", "32");
+		c.storeData("Stream", "mca");
+		c.storeData("Subject", "Java");
+		
+		System.out.println(c);
+		
+		System.out.println(c.getData("Roll"));
+		System.out.println(c);
+		System.out.println(c.getData("Name"));
+		System.out.println(c);
+	}
+}
---
+++ src/test/java/com/cache/lru/CacheTest.java
@@ -0,0 +1,24 @@
+package com.cache.lru;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CacheTest {
+
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStoreData() {
+		fail("Not yet implemented");
+	}
+
+}
